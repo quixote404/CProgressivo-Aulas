9cab40e799bb255e48cadfcd78241ef3da90f188
rosegaze <mrp.ioerror@gmail.com>
2022-06-27 16:46:55 -0300

    Adicionando os arquivos

---
+++ Aula-01/aula-01.c
@@ -0,0 +1,23 @@
+/*
+#------------------------------------------------------------------
+# Sobre a criação
+#
+# Autor       : Matheus da Rosa - Quixotte
+# Repositório : https://github.com/quixote404
+# Email       : hologramrp.@proton.me
+#
+#------------------------------------------------------------------
+# Sobre o programa
+#
+# Versão      : 0.1  -  23/06/2022
+# Sobre       : Aula 03 - Comentário no Código
+# License     : Treinamento
+#------------------------------------------------------------------*/
+#include <stdio.h>
+
+int main (void){
+
+	printf("Haro, navy! \n");
+	return 0;
+	
+}
