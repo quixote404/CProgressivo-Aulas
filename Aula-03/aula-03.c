c25f18780ff09949f402ddb8cf7dfe7e704da0e4
rosegaze <mrp.ioerror@gmail.com>
2022-06-28 18:43:04 -0300

    Ajustando binario em C

--- Aula-03/aula-03.c
+++ Aula-03/aula-03.c
@@ -10,15 +10,28 @@
 # Sobre o programa
 #
 # Versão      : 0.1  -  27/06/2022
-# Sobre       : Aula 03 - Comentário no Código
+# Sobre       : Aula 03 - Exibição e declaração de valores inteiros
 # License     : Treinamento
 #------------------------------------------------------------------*/
 
 #include <stdio.h>
 
+/* Declarando Variáveis */
+
+// No C, declara-se ou a variável informando apenas o tipo:
+int resets;
+
+//Ou já se informa o valor
+int contador = 10;
+
+
+//Função principal
 int main (void){
 
-	printf("Haro, navy! \n");
+	//Podendo se informar o valor da variável dentro da função
+	resets = 4;
+	
+	printf("Já tem %d dias desde o último reset no servidor!\nQuantidade de resets: %d\n", contador, resets);
 	return 0;
 	
 }
